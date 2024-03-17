538fb2b0c2fd3b242d9a75194dc12334fa082427
Mijndert Stuij <mijndert@mijndertstuij.nl>
2024-03-17 21:43:05 +0100

    adds rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = config => {
   config.addFilter("dateFilter", dateFilter);
   config.addFilter("w3DateFilter", w3DateFilter);
   config.addPassthroughCopy("css");
+  config.addPlugin(pluginRss);
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
