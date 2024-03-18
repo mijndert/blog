c63fd5cd9ca4c5a285ed16731d85beabaf4b6d15
Mijndert Stuij <mijndert@mijndertstuij.nl>
2024-03-18 16:18:44 +0100

    adds blogposts to homepage, fixes rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -2,18 +2,24 @@ const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-module.exports = config => {
+module.exports = (config) => {
   config.addFilter("dateFilter", dateFilter);
   config.addFilter("w3DateFilter", w3DateFilter);
+  config.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+    return array.slice(0, n);
+  });
   config.addPassthroughCopy("css");
   config.addPlugin(pluginRss);
   return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
-      input: 'src',
-      output: 'dist'
-    }
+      input: "src",
+      output: "dist",
+    },
   };
 };
--- src/index.md
+++ src/index.md
@@ -3,8 +3,18 @@ title: 'Vision'
 layout: 'layouts/page.html'
 ---
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/a0bb883b-9b5c-4235-93f0-ab66ce0651b8/deploy-status)](https://app.netlify.com/sites/mijndert-vision/deploys)
-
 Hi there!
 
-[rss](/feed.xml)
+<ul role="list">
+  {%- for post in collections.post | reverse | head(-5) -%}
+  <h2><a href="{{ post.url }}">{{ post.data.title }}</a></h2>
+  <time datetime="{{ post.date | w3DateFilter }}" class="small">
+    {{ post.date | dateFilter }}
+  </time>
+  <p class="font-serif">{{ post.data.summary }}</p>
+  {%- endfor -%}
+</ul>
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/a0bb883b-9b5c-4235-93f0-ab66ce0651b8/deploy-status)](https://app.netlify.com/sites/mijndert-vision/deploys)
+
+[rss feed](/feed.xml)
